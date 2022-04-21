c49f9cb4b26c0fc8961983c3a87f738cabd7b67d
Dorialean <lana.lakutina@mail.ru>
2022-04-21 10:36:37 +0300

    bootstrap 5

--- CardsWorld/settings.py
+++ CardsWorld/settings.py
@@ -127,10 +127,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, ''),
+    os.path.join('static/'),
+    os.path.join('boot/')
 ]
 
 # Default primary key field type
